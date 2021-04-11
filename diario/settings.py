d5e2768f25aa9ae006146cca3064c6e3746f9b64
Felipe <narulee@hotmail.com>
2021-04-11 14:03:43 -0300

    icons

--- diario/settings.py
+++ diario/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "dashboard"
 
